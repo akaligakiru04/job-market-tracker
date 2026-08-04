f8b0fb7b29bf10af28734af8dacbdb0e117730eb
Farah Beldjoudi <farahbeldjoudi04@gmail.com>
2026-08-04 13:21:15 +0200

    added root route

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Maret Tracker API")
 
+@app.get("/")
+def root():
+    return {
+        "service" : "Market tracker API"
+        "status" : "under construction"
+        "docs" : "/docs"
+    }
+
 @app.get("/health")
 def health():
     return {"status" : "ok"}
